c97101de4288fc3b7bdb6cdf6807f7e068f9964c
Shreyas <helloshreyasj@gmail.com>
2026-06-02 17:59:23 +0400

    feat: add restaurant listing table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatSelect, MatOption } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
+import { Restaurant } from './restaurant.model';
+import { ReactiveFormsModule } from '@angular/forms';
+ReactiveFormsModule
 
 @Component({
   selector: 'app-root',
-  imports: [MatFormField, MatLabel, MatOption, MatSelect, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, MatIconModule, MatTableModule],
+  imports: [ MatFormField, MatLabel, MatOption, MatSelect, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, MatIconModule, MatTableModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  displayedColumns: string[] = ['name', 'location', 'priceRange', 'rating', 'actions'];
+  restaurantsList = signal<Restaurant[]>([]);
 }
---
+++ src/app/restaurant.model.ts
@@ -0,0 +1,6 @@
+export interface Restaurant {
+    name: string;
+    location: string;
+    priceRange: string;
+    rating: number;
+}
